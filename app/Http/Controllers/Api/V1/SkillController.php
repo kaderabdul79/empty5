3487e175750ad13df0d138a3479148b9ae5df303
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-08-17 06:19:51 +0600

    insert new skill and validate data also created request class for validation

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
+use App\Models\Skills;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSkillRequest;
 
 class SkillController extends Controller
 {
     //
     public function index(){
         return response()->json("skill index");
+    }
+    // 
+    public function store(StoreSkillRequest $request)
+    {
+        Skills::create($request->validated());
+        return response()->json("Skill Created");
     }
 }
