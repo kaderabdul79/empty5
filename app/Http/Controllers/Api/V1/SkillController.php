7868c242660f035af09cbfa72cf5c317f2f97302
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-08-17 06:19:51 +0600

    created resource and collection and updated the skills

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -6,17 +6,32 @@ use App\Models\Skills;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSkillRequest;
+use App\Http\Resources\V1\SkillResource;
+use App\Http\Resources\V1\SkillCollection;
 
 class SkillController extends Controller
 {
     //
     public function index(){
-        return response()->json("skill index");
+        return new SkillCollection(Skills::all());
     }
-    // 
+    // insert skill
     public function store(StoreSkillRequest $request)
     {
         Skills::create($request->validated());
         return response()->json("Skill Created");
+    }
+
+    // 
+    public function show(Skills $skills)
+    {
+        return new SkillResource($skills);
+    }
+
+    // update skill
+    public function update(StoreSkillRequest $request, Skills $skills)
+    {
+        $skills->update($request->validated());
+        return response()->json("Skill Updated");
     }
 }
